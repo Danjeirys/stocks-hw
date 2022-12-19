95ce0db385c4b4784a2c8d613f40819c493e37a5
Jerry Martinez <jaysavageg@gmail.com>
2022-12-19 16:21:38 -0500

    work in progress

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About () {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper risus in hendrerit gravida rutrum quisque non. Nunc sed velit dignissim sodales ut eu. Aliquam eleifend mi in nulla posuere sollicitudin aliquam. Habitant morbi tristique senectus et netus et. Ut sem viverra aliquet eget sit amet tellus. Nisi est sit amet facilisis magna etiam tempor orci. Nec ullamcorper sit amet risus nullam eget. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Morbi tincidunt augue interdum velit. Non consectetur a erat nam at lectus urna duis convallis. Augue mauris augue neque gravida. Sed euismod nisi porta lorem mollis. Tristique senectus et netus et malesuada fames ac turpis egestas. Mauris sit amet massa vitae tortor. Velit ut tortor pretium viverra.</p>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus egestas tellus rutrum tellus pellentesque. Orci dapibus ultrices in iaculis nunc. Sagittis eu volutpat odio facilisis mauris sit amet massa vitae. Amet risus nullam eget felis. Est velit egestas dui id ornare arcu odio. Elit at imperdiet dui accumsan sit amet nulla. Aliquam ultrices sagittis orci a scelerisque purus. Tristique senectus et netus et malesuada fames ac turpis. Tristique sollicitudin nibh sit amet. Fusce ut placerat orci nulla pellentesque. Senectus et netus et malesuada fames ac turpis egestas. Aliquet nec ullamcorper sit amet risus nullam eget felis.</p>
+        </div>
+    )
+}
